75a2910766985a674c3bd3962b9f308452721791
Akshat Aman <aman.akshat07@gmail.com>
2019-06-28 00:04:37 +0530

    Update CollectionUtil.java

--- Billing_Application2/src/com/capgemini/takehome/util/CollectionUtil.java
+++ Billing_Application2/src/com/capgemini/takehome/util/CollectionUtil.java
@@ -6,8 +6,8 @@ import com.capgemini.takehome.bean.Product;
 public class CollectionUtil {
 	private static Map<Integer,Product> products=new HashMap<Integer,Product>();
 	static {
-		products.put(1002, new Product(1001,"iphone","electronics",35000));
-		products.put(1001, new Product(1002,"ledtv","electronics",45000));
+		products.put(1001, new Product(1001,"iphone","electronics",35000));
+		products.put(1002, new Product(1002,"ledtv","electronics",45000));
 		products.put(1003, new Product(1003,"teddy","toy",800));
 		products.put(1004, new Product(1004,"telescope","toy",5000));
 	}
